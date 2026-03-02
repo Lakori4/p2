05712452bc5881cd46bc78fb28145506222d466f
eric <eric2004_24@hotmail.com>
2026-03-02 10:17:23 +0100

    jio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 "use client"
 import CountryCard from "@/components/CountryCard";
-import { getAllCountries, getCountryByName } from "@/lib/api";
+import { getAllCountries } from "@/lib/api";
 import { Country } from "@/lib/types";
 import { useEffect, useState } from "react";
 
 export default function Home() {
 
   const [countries, setCountries] = useState<Country[]|null>(null);
-  const [busqueda, setBusqueda]= useState("")
+  const [busqueda, setBusqueda] = useState("")
 
   useEffect(()=>{
     getAllCountries().then((data)=>{
       setCountries(data)
     })
-  })
+  }, []); // <--- El arreglo vacío evita un bucle infinito
 
  const filter = countries?.filter((country)=>(country.name.common.toLowerCase().includes(busqueda.toLowerCase())))
 
@@ -22,11 +22,24 @@ export default function Home() {
 
       <h1>Explorador de Países</h1>
       
+      <input 
+        type="text" 
+        placeholder="Buscar un país..." 
+        value={busqueda} 
+        onChange={(e) => setBusqueda(e.target.value)} 
+      />
+
       <div>
-        {filter?.map((country)=>(<CountryCard key={country.name.common} country={country}></CountryCard>))}
+        {!countries ? (
+          <p>Cargando países...</p>
+        ) : filter?.length === 0 ? (
+          <p>No se encontraron resultados.</p>
+        ) : (
+          filter?.map((country)=>(
+            <CountryCard key={country.name.common} country={country}></CountryCard>
+          ))
+        )}
       </div>
-
-
 
     </div>
   );
